68d17c93d066b2507afc504eecd45bff5e5be926
Manav Panchal <manav03panchal@gmail.com>
2023-06-01 12:11:03 -0600

    more cleaning. now working on making an order sys

--- alpacaStuff.py
+++ alpacaStuff.py
@@ -1,6 +1,5 @@
 import keys
 import time
-import requests
 from alpaca.data.historical import StockHistoricalDataClient
 from alpaca.data.requests import StockLatestQuoteRequest
 from alpaca.data.live import StockDataStream
@@ -19,10 +18,11 @@ def showCryptoLivePrice(userStockQueryParam):
     request_paramsCrypto = CryptoLatestQuoteRequest(symbol_or_symbols=[f'{userStockQueryParam}'])
     try:
         while True:
-            # time.sleep(5)
+            
             latest_quote = client.get_crypto_latest_quote(request_paramsCrypto)
             latest_ask_priceCrypto = latest_quote[f'{userStockQueryParam}'].ask_price
             print(f"{userStockQueryParam}: ${latest_ask_priceCrypto}",end="\r")
+            time.sleep(2)
     # must use symbol to access even though it is single symbol
     except KeyboardInterrupt:
             print(f"\n{userStockQueryParam} : ${latest_ask_priceCrypto}")
@@ -35,10 +35,11 @@ def showStockLivePrice(userStockQueryParam):
     symbol_quotes = client.get_stock_latest_quote(request_params)
     try:
         while True:
-            # time.sleep(5)
+            
             symbol_quotes = client.get_stock_latest_quote(request_params)
             latest_ask_price = symbol_quotes[f'{userStockQueryParam}'].ask_price
             print(f"{userStockQueryParam} : ${latest_ask_price}",end="\r")
+            time.sleep(2)
     except KeyboardInterrupt:
             print(f"\n{userStockQueryParam} : ${latest_ask_price}")
 
@@ -53,24 +54,21 @@ def showStockLivePrice(userStockQueryParam):
 
 # wss_client.run()
 
+def getLivePriceAskAsset(trading_client):
+    try:
+        userStockQueryParam = input("Enter the Ticker Symbol: ")
+        try:
+            if trading_client.get_asset(userStockQueryParam).tradable:
+                showStockLivePrice(userStockQueryParam)
+        except:
+                showCryptoLivePrice(userStockQueryParam)
+    except:
+            print("This particular security is not tradable or doesn't exist.")
+
 def main():
     trading_client = TradingClient(keys.KEY, keys.SECRET)
-
-# search for crypto assets
-    search_params = GetAssetsRequest(asset_class=AssetClass.CRYPTO)
-
-    assets = trading_client.get_all_assets(search_params)
-    # Adding all tradable crypto assets into a list so we can have one single if else statement to find the price for 
-    # any asset, be it crypto or stocks.
-    allCryptoSymbols = []
-    for i in range(len(assets)):
-        allCryptoSymbols.append(assets[i].symbol)
-    userStockQueryParam = input("Enter the Ticker Symbol: ")
-    if trading_client.get_asset(userStockQueryParam).tradable:
-        showStockLivePrice(userStockQueryParam)
-    elif userStockQueryParam in allCryptoSymbols:
-        showCryptoLivePrice(userStockQueryParam)
-    else:
-        print("This particular security is not tradable or doesn't exist.")
+    if (trading_client.get_clock().is_open):
+        print("The market is open.")
+    getLivePriceAskAsset(trading_client)
 
 main()
