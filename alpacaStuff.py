84167d2c905abd41670a99ae7feac497d860b812
Manav Panchal <manav03panchal@gmail.com>
2023-06-01 09:00:39 -0600

    now trying to make single query to get askprice

--- alpacaStuff.py
+++ alpacaStuff.py
@@ -11,10 +11,6 @@ from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import GetAssetsRequest
 from alpaca.trading.enums import AssetClass
 
-
-
-
-
 def showCryptoLivePrice(userStockQueryParam):
     # no keys required
     client = CryptoHistoricalDataClient()
@@ -30,25 +26,21 @@ def showCryptoLivePrice(userStockQueryParam):
     # must use symbol to access even though it is single symbol
     except KeyboardInterrupt:
             print(f"\n{userStockQueryParam} : ${latest_ask_priceCrypto}")
-            pass
 
+def showStockLivePrice(userStockQueryParam):
+    # # keys required for stock historical data client
+    client = StockHistoricalDataClient(keys.KEY, keys.SECRET)
 
-
-# # # keys required for stock historical data client
-# client = StockHistoricalDataClient(keys.KEY, keys.SECRET)
-
-# # # multi symbol request - single symbol is similar
-# userStockQueryParam = input("Enter the Ticker Symbol: ")
-# request_params = StockLatestQuoteRequest(symbol_or_symbols=[f'{userStockQueryParam}'])
-# symbol_quotes = client.get_stock_latest_quote(request_params)
-
-# while True:
-#     # time.sleep(5)
-#     symbol_quotes = client.get_stock_latest_quote(request_params)
-#     latest_ask_price = symbol_quotes[f'{userStockQueryParam}'].ask_price
-#     print(f"The latest asking price for the security is: ${latest_ask_price}",end="\r")
-
-
+    request_params = StockLatestQuoteRequest(symbol_or_symbols=[f'{userStockQueryParam}'])
+    symbol_quotes = client.get_stock_latest_quote(request_params)
+    try:
+        while True:
+            # time.sleep(5)
+            symbol_quotes = client.get_stock_latest_quote(request_params)
+            latest_ask_price = symbol_quotes[f'{userStockQueryParam}'].ask_price
+            print(f"{userStockQueryParam} : ${latest_ask_price}",end="\r")
+    except KeyboardInterrupt:
+            print(f"\n{userStockQueryParam} : ${latest_ask_price}")
 
 # wss_client = StockDataStream(keys.KEY, keys.SECRET)
 
@@ -68,13 +60,17 @@ def main():
     search_params = GetAssetsRequest(asset_class=AssetClass.CRYPTO)
 
     assets = trading_client.get_all_assets(search_params)
+    # Adding all tradable crypto assets into a list so we can have one single if else statement to find the price for 
+    # any asset, be it crypto or stocks.
     allCryptoSymbols = []
     for i in range(len(assets)):
         allCryptoSymbols.append(assets[i].symbol)
     userStockQueryParam = input("Enter the Ticker Symbol: ")
-    if userStockQueryParam in allCryptoSymbols:
+    if trading_client.get_asset(userStockQueryParam).tradable:
+        showStockLivePrice(userStockQueryParam)
+    elif userStockQueryParam in allCryptoSymbols:
         showCryptoLivePrice(userStockQueryParam)
     else:
-        print("DNE.")
+        print("This particular security is not tradable or doesn't exist.")
 
 main()
